6bac8a5a184cd230d74ff746865ff39c269298aa
romerorodriguez <a3522110182@alumno.uttehuacan.edu.mx>
2024-02-28 10:34:31 -0600

    dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados-jrr';
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
+  numero1: number = 1;
+  numero2: number = 1;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+    this.dadoIzquierdo = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '../assets/img/dice' + this.numero2 + '.png';
+
+    // console.log(this.numero1);
+    // console.log(this.numero2);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarJrrComponent } from './components/navbar-jrr/navbar-jrr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarJrrComponent
   ],
   imports: [
     BrowserModule
